68bfd4b9771eaf878e05901a03159587e5660288
Christopher Stock <email@christopherstock.de>
2018-10-31 21:44:32 +0100

    Removed music. Outsourced JS from index.html. Removed fps library.

--- common.js
+++ common.js
@@ -130,7 +130,6 @@ var Game = {  // a modified version of the game loop from my previous boulderdas
         requestAnimationFrame(frame, canvas);
       }
       frame(); // lets get this party started
-      Game.playMusic();
     });
   },
 
@@ -199,22 +198,6 @@ var Game = {  // a modified version of the game loop from my previous boulderdas
     }, 5000);
     return result;
   },
-
-  //---------------------------------------------------------------------------
-
-  playMusic: function() {
-    var music = Dom.get('music');
-    music.loop = true;
-    music.volume = 0.05; // shhhh! annoying music!
-    music.muted = (Dom.storage.muted === "true");
-    music.play();
-    Dom.toggleClassName('mute', 'on', music.muted);
-    Dom.on('mute', 'click', function() {
-      Dom.storage.muted = music.muted = !music.muted;
-      Dom.toggleClassName('mute', 'on', music.muted);
-    });
-  }
-
 }
 
 //=========================================================================
