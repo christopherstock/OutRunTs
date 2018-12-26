7d72f2906e397c00e52c6770c2c61d955d1b9ab4
Christopher Stock <christopher.stock@mayflower.de>
2018-12-26 22:00:36 +0300

    Moved images to different folders. Preparing image sprite replacement by adding 'src' attribute to all sprites.

--- src/typescript/de/mayflower/orts/legacy/Background.ts
+++ src/typescript/de/mayflower/orts/legacy/Background.ts
@@ -4,7 +4,7 @@
     *******************************************************************************************************************/
     export class BACKGROUND
     {
-        static HILLS = { x:   5, y:   5, w: 1280, h: 480 };
-        static SKY   = { x:   5, y: 495, w: 1280, h: 480 };
-        static TREES = { x:   5, y: 985, w: 1280, h: 480 };
+        static HILLS = { x:   5, y:   5, w: 1280, h: 480, src: 'background/hills.png' };
+        static SKY   = { x:   5, y: 495, w: 1280, h: 480, src: 'background/sky.png'   };
+        static TREES = { x:   5, y: 985, w: 1280, h: 480, src: 'background/trees.png' };
     }
